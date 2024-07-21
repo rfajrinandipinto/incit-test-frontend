120792fe1782b09c44366151ef7eaa01cd38d555
rfajrinandipinto <nandipintorahmatfajri@gmail.com>
2024-07-21 17:59:02 +0700

    test

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -482,7 +482,7 @@ export default function Example() {
                                                         </thead>
                                                         <tbody className="divide-y divide-gray-200 bg-white">
                                                             {users.map((user: User) => (
-                                                                <tr >
+                                                                <tr key={user.name} >
                                                                     <td className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">
                                                                         {user.name || 'No Name Provided'}
                                                                     </td>
